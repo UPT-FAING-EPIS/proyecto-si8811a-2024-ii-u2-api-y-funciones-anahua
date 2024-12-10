2a01f564aa8a0557a03b530a06a85f79648c17c1
Mayner <ma2020067145@virtual.upt.pe>
2024-12-09 22:56:19 -0500

    Estructura separada

--- fast-point-api/probando.py
+++ fast-point-api/probando.py
@@ -6,6 +6,9 @@ from fast_point_api.crud import create_item
 
 clase = CouchDBManager('http://admin:admin@localhost:5984')
 
+'''
+
+
 a = clase.get_existing_collections()
 
 b = clase.get_db('_users')
@@ -33,3 +36,45 @@ from fast_point_api.crud import update_item
 updated_data = {"price": 99.99}
 updated_item = update_item(clase, "pizzas", "8468bc67-d09f-4e73-981b-061783ff44a6", updated_data)
 print(updated_item)
+
+
+'''
+
+
+# FUNCION CREAR_BD_COUCHDB() ???
+#docker run -d -p 5984:5984 -e COUCHDB_USER=admin -e COUCHDB_PASSWORD=admin couchdb
+#docker run -d -p 5985:5984 -e COUCHDB_USER=admin -e COUCHDB_PASSWORD=admin couchdb
+#docker run -d -p 5986:5984 -e COUCHDB_USER=admin -e COUCHDB_PASSWORD=admin couchdb
+
+
+
+# uvicorn api_lugares.main:app --reload
+# uvicorn fastapi-couchdb-component.main:app --reload
+
+
+
+from fast_point_api.app_factory import create_app
+from fast_point_api.schemas import PizzaModel, DrinkModel
+from fast_point_api.endpoints import generar_endpoints_genericos
+
+#Evitar que use "_users" u otra db del sistema de couchdb
+#app = create_app(connection_url="http://admin:admin@localhost:5984")
+
+
+
+collections = {
+    "pizzas": PizzaModel,
+    "bebidas": DrinkModel,
+}
+#Endpoints con la conexion 85
+app = create_app(
+    connection_url="http://admin:admin@localhost:5985",
+    models_dict=collections
+    )
+
+
+#Endpoints con la conexion 84
+a = generar_endpoints_genericos(app,clase)
+
+
+#Asi la api funcionara con la bd segun la conexion que se desee como puede ser una 86 y etc
