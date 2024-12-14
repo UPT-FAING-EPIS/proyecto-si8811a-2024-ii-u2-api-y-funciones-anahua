ef1824144b848401697f4c15461b6b1869441054
Mayner <ma2020067145@virtual.upt.pe>
2024-12-14 10:51:52 -0500

    Arreglos en probando.py

--- fast-point-api/probando.py
+++ fast-point-api/probando.py
@@ -3,45 +3,12 @@ from fast_point_api.config import CouchDBManager
 from fast_point_api.schemas import PizzaModel
 from fast_point_api.crud import create_item
 
+#uvicorn probando:app --reload
 
 clase = CouchDBManager('http://admin:admin@localhost:5984')
 
-'''
 
-
-a = clase.get_existing_collections()
-
-b = clase.get_db('_users')
-b = clase.get_db('vdfveee')
-d = clase.get_db('asasas')
-e = clase.get_db('rerfer')
-
-c= clase.delete_collection('vfveee')
-
-#f1 = clase.delete_all_collections(exclude_system_dbs = True)
-#f2 = clase.delete_all_collections(exclude_system_dbs = False)
-
-g = clase.get_existing_collections()
-
-#print(a)
-#print(b)
-
-
-pizza_data = PizzaModel(name="Margarita", description="Pizza clásica", ingredients="Tomate, queso", size="Mediana", price=12.99, is_vegetarian=True)
-#new_item = create_item(clase, "pizzas", pizza_data)
-#print(new_item)
-
-from fast_point_api.crud import update_item
-
-updated_data = {"price": 99.99}
-updated_item = update_item(clase, "pizzas", "8468bc67-d09f-4e73-981b-061783ff44a6", updated_data)
-print(updated_item)
-
-
-'''
-
-
-# FUNCION CREAR_BD_COUCHDB() ???
+# INSTANCIAS DE COUCHDB
 #docker run -d -p 5984:5984 -e COUCHDB_USER=admin -e COUCHDB_PASSWORD=admin couchdb
 #docker run -d -p 5985:5984 -e COUCHDB_USER=admin -e COUCHDB_PASSWORD=admin couchdb
 #docker run -d -p 5986:5984 -e COUCHDB_USER=admin -e COUCHDB_PASSWORD=admin couchdb
